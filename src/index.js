df5b23328d9732d5380ca3b9aeaeb46e7aa96759
Firoz Akhter <firozakhter789@gmail.comgit config --list>
2025-12-25 18:06:06 +0530

    initial-commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+
+const app = express();
+
+app.use(cors());
+app.use(morgan("dev"));
+
+require("dotenv").config();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "Node.js server is running 🚀" });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
